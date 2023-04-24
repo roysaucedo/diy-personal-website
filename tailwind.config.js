f8bc48487577dc1c20a314a7ebd8d8f9f752573c
roysaucedo <roy.a.saucedo@gmail.com>
2023-04-24 13:43:01 -0700

    content-added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'profile-image': "url('/src/img/profile-img.PNG')"
+      }
+    },
   },
   plugins: [],
 }
