cf29e172bcde51c7de44756e17ab428827a24104
roysaucedo <roy.a.saucedo@gmail.com>
2023-04-26 16:44:07 -0700

    color theme

--- src/site-content.js
+++ src/site-content.js
@@ -1,25 +1,43 @@
-fetch('/src/site-content.json')
-  .then(response => response.json())
-  .then(data => {
-    const imageElement = document.getElementById('profile-img');
+fetch("/src/site-content.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const imageElement = document.getElementById("profile-img");
     const pageImage = data.img;
     imageElement.src = pageImage;
 
-    const titleElement = document.getElementById('title');
+    const titleElement = document.getElementById("title");
     const pageTitle = data.title;
     titleElement.textContent = pageTitle;
 
-    const summaryElement = document.getElementById('summary');
+    const summaryElement = document.getElementById("summary");
     const pageSummary = data.summary;
     summaryElement.textContent = pageSummary;
 
     const container = document.getElementById("linksContainer");
     data.links.forEach((link) => {
-        const linkElement = document.createElement("a");
-        linkElement.href = link.url;
-        linkElement.textContent = link.title;
-        linkElement.classList.add("w-full", "p-4", "my-4", "bg-blue-500", "text-white", "rounded-lg", "shadow-lg", "block");
-        container.appendChild(linkElement);
+      const linkElement = document.createElement("a");
+      linkElement.href = link.url;
+      linkElement.textContent = link.title;
+      linkElement.classList.add(
+        "w-full",
+        "p-4",
+        "my-4",
+        "text-white",
+        "bg-black",
+        "rounded-lg",
+        "shadow-lg",
+        "block",
+        "hover:scale-105",
+        "ease-in-out",
+        "duration-300"
+      );
+      container.appendChild(linkElement);
     });
-});
 
+    const colorScheme = data.colorScheme;
+    const bgColorClass = `bg-${colorScheme}-300`;
+    const background = document.getElementById("background");
+
+    // Apply the classes to the relevant elements
+    background.classList.add(bgColorClass);
+  });
